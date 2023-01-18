2be979c8be7d9ca4f6ea346cd609c7e95b951723
gmekv <113336356+gmekv@users.noreply.github.com>
2023-01-18 14:49:48 +0400

    Initial Commit

---
+++ Explore Georgia/Explore_GeorgiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Explore_GeorgiaApp.swift
+//  Explore Georgia
+//
+//  Created by Giorgi Meqvabishvili on 18.01.23.
+//
+
+import SwiftUI
+
+@main
+struct Explore_GeorgiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
